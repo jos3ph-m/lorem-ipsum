4bfd31105c62897e5ab1c2d2c637e022a0a70a3b
Joseph M <jmedina1985@hotmail.com>
2021-03-15 22:08:58 -0700

    getting set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}></form>
+    </section>
+  );
 }
 
 export default App;
